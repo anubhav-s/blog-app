af4714e0ccb041be1c4d5a05ce78f9507091673a
Anubhav Sengar <anubhavs.rns@gmail.com>
2019-05-19 21:43:27 +0530

    change in model

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,10 @@ class PublishedManager(models.Manager):
 
 
 class BlogDb(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, default=None, null=True, on_delete=models.CASCADE)
+    """
+    This is model class which is used for database creation.
+    """
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, default=None, null=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
